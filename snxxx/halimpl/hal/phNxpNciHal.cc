3a3c582de1755b3e6eee9ac76b96e7bdeeca34c2
nxp61433 <siddaram.khed@nxp.com>
2022-09-27 14:49:11 +0530

    [artf987241][FW DNLD]:Firmware download sequence adaptation
    LPCD feature not support and blocked.

--- snxxx/halimpl/hal/phNxpNciHal.cc
+++ snxxx/halimpl/hal/phNxpNciHal.cc
@@ -3581,7 +3581,7 @@ NFCSTATUS phNxpNciHal_resetDefaultSettings(uint8_t fw_update_req,
     int ret = 0;
 #if (NXP_EXTNS != TRUE)
     phNxpNciHal_conf_nfc_forum_mode();
-#endif
+
     if (nfcFL.chipType >= sn100u) {
       ret = GetNxpNumValue(NAME_NXP_RDR_DISABLE_ENABLE_LPCD, &num, sizeof(num));
       if (!ret || num == 1 || num == 2) {
@@ -3590,6 +3590,8 @@ NFCSTATUS phNxpNciHal_resetDefaultSettings(uint8_t fw_update_req,
         phNxpNciHal_prop_conf_lpcd(false);
       }
     }
+
+#endif
   }
   return status;
 }
